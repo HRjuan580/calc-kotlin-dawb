f34ce8773f8b47964a160f13f52958cfd18abf55
HRJuan580 <jherrel580@g.educaand.es>
2025-03-13 16:06:25 +0100

    Commit inicial

--- src/Calc.kt
+++ src/Calc.kt
@@ -21,6 +21,7 @@ fun multiplica(num1: Int, num2: Int): Int {
     return num1 * num2
 }
 
+
 fun showResult(operation: String, result: Int) {
     println("El resultado de la operación \"$operation\" es: $result")
 }
